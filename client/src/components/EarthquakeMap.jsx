3c1e1eb22f58db9d3a3f52d56b22316bd2566df9
Hookspike <Hookspike@outlook.com>
2026-06-08 15:28:49 +0800

    Switch map tile server to CARTO for better performance

--- client/src/components/EarthquakeMap.jsx
+++ client/src/components/EarthquakeMap.jsx
@@ -35,8 +35,9 @@ function EarthquakeMap({ earthquakes }) {
           style={{ height: '100%', width: '100%' }}
         >
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            maxZoom={19}
           />
           {earthquakes.features.map((earthquake) => {
             const [longitude, latitude] = earthquake.geometry.coordinates;
